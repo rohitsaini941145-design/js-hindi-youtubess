3e80862f95f46607acb8456c8ccf821f5d43584e
Rohit Saini <rohitsaini941145@gmail.com>
2026-03-24 18:10:32 +0000

    container config done

---
+++ 01_basics/01_varibles.js
@@ -0,0 +1,10 @@
+const accountId = 12222
+let accountEmial = "amansainiofficial.com"
+var accountPassword = "123445"
+accountcity = "jaipur"
+
+accountEmial = "sainiaman"
+accountPassword = "22222"
+accountcity = "gangoh"
+
+console.table([accountEmial, accountcity, accountId, accountPassword])
